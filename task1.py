4b32c5f72a4aff409bb6fed2779607d6136115f7
gracelo94 <graceloo94@1utar.edu.my>
2016-03-11 02:18:12 +0800

    Update

--- task1.py
+++ task1.py
@@ -3,7 +3,7 @@ import numpy as np
 #You can import some modules or create additional functions
 ITERATION_LIMIT = 10
 
-def LUdecomp(A):
+def LU_decomp(A):
        n=len(A)
        for k in range(0, n-1):
         for i in range(k+1, n):
@@ -14,7 +14,7 @@ def LUdecomp(A):
         return A
 
 def lu(A,b):
-      A=LUdecomp(A)
+      A=LU_decomp(A)
       n = len(A)
       for k in range(1,n):
            b[k] = b[k] - np.dot(A[k,0:k], b[0:k])
@@ -27,17 +27,58 @@ def lu(A,b):
 def sor(A, b):
     sol = []
     # Edit here to implement your code
-    omega = 1.03
+    #A = D-L-U
+    D = [[0 for x in range (0,n)] for x in range (0,n)]
+    L = [[0 for x in range (0,n)] for x in range (0,n)]
+    U = [[0 for x in range (0,n)] for x in range (0,n)]
+    invD = [[0 for x in range (0,n)] for x in range (0,n)]
+    T = [[0 for x in range (0,n)] for x in range (0,n)]
+   
+    #compute diagonal matrix D
+    for i in range (0, n):
+        D[i][i] = A[i][i]
     
+    #compute lower triangular matrix L    
+    for i in range (1, n):
+        for j in range (0, i):
+            L[i][j] = -A[i][j]
+    
+    #compute upper triangular U        
+    for i in range (0, n):
+        for j in range(i+1, n):
+            U[i][j] = -A[i][j]
+    
+    #compute inverse matrix of D, named invD
+    for i in range (0, n):
+        invD[i][i] = 1/D[i][i]
+    
+    #compute matrix T, where T = invD * (L + U)
+    for i in range(0, n):
+        for j in range(0, n):
+            for k in range(0, n):
+                T[i][j] += invD[i][k]*(L[k][j] + U[k][j])
+                
+    #compute the spectral radius of matrix T, named pT, where pT = max(all eigenvalues)
+    pT = max(abs(np.linalg.eigvals(T)))
+    
+    #compute optimal omega for SOR method
+    omega = 2*(1 - np.sqrt(1 - ((pT)**2)))/((pT)**2)
+
     x = np.zeros_like(b)
     for itr in range(ITERATION_LIMIT):
         for j in range(len(b)):
             sums = np.dot( A[j,:], x )
             x[j] = x[j] + omega*(b[j]-sums)/A[j,j]
+
     return list(sol)
 
 def solve(A, b):
-    condition = np.count_nonzero(A) > 1/2 *len(A)    # State and implement your condition here
+    condition = True # State and implement your condition here
+    condition = False
+    try:       
+        np.linalg.cholesky(A)
+    except np.linalg.linalg.LinAlgError :
+        condition = True    
     if condition:
         print('Solve by lu(A,b)')
         return lu(A,b)
@@ -51,7 +92,7 @@ if __name__ == "__main__":
 
     A = np.array([[2,1,6], [8,3,2], [1,5,1]]).astype(float)
     b = np.array([9, 13, 7]).astype(float)
-    
+    n = len(A)
     sol = np.linalg.solve(A,b)
     solve(A,b)
     print(sol)
@@ -63,7 +104,7 @@ if __name__ == "__main__":
          [-3782, 3840, 2464, -8389, 9781,-3334],
          [-6903, 5610, 4306, 5548, -1380, 3539.]]).astype(float)
     b = np.array([ 17603,  -63286,   56563,  -26523.5, 103396.5, -27906]).astype(float)
-       
+    n=len(A)  
     sol = np.linalg.solve(A,b)
     solve(A,b)
     print(sol)
--- task2.py
+++ task2.py
@@ -1,11 +1,52 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import scipy as sp
 
-img=mpimg.imread('Lenna.tiff')
+
+def svd_img(n): #define function svd_img
+    
+    #creating new sigma matrix for red, green, blue
+    Sigma_red=Sred.copy()
+    Sigma_green=Sgreen.copy()
+    Sigma_blue=Sblue.copy()
+
+    # to keep the 30 non-zero elements
+    Sigma_red[n:800]=np.zeros_like(Sred[n:800])
+    Sigma_green[n:800]=np.zeros_like(Sgreen[n:800])
+    Sigma_blue[n:800]=np.zeros_like(Sblue[n:800])
+
+    # to create diagonal matrix to perform dot multiplication
+    Sigma_red = sp.linalg.diagsvd(Sigma_red,800,1000)
+    Sigma_green = sp.linalg.diagsvd(Sigma_green,800,1000)
+    Sigma_blue = sp.linalg.diagsvd(Sigma_blue,800,1000)
+
+    # to perform dot multiplication for new matrix
+    Red_new = np.dot(np.dot(Ured,Sigma_red), Vred)
+    Green_new = np.dot(np.dot(Ugreen,Sigma_green), Vgreen)
+    Blue_new = np.dot(np.dot(Ublue,Sigma_blue), Vblue) 
+
+    #the new resolution matrix
+    img[:,:,0]= Red_new
+    img[:,:,1]= Green_new
+    img[:,:,2]= Blue_new
+
+    #to plot the images
+    fig2 = plt.figure(n)
+    ax1 = fig2.add_subplot(2,2,1)
+    ax2 = fig2.add_subplot(2,2,2)
+    ax3 = fig2.add_subplot(2,2,3)
+    ax4 = fig2.add_subplot(2,2,4)
+    ax1.imshow(img)
+    ax2.imshow(r, cmap = 'Reds')
+    ax3.imshow(g, cmap = 'Greens')
+    ax4.imshow(b, cmap = 'Blues')
+    plt.show()
+
+
+#the original image
+img=mpimg.imread('Watchtower_coast.jpg')
 [r,g,b] = [img[:,:,i] for i in range(3)]
-
-
 fig = plt.figure(1)
 ax1 = fig.add_subplot(2,2,1)
 ax2 = fig.add_subplot(2,2,2)
@@ -17,3 +58,24 @@ ax3.imshow(g, cmap = 'Greens')
 ax4.imshow(b, cmap = 'Blues')
 plt.show()
 
+#to find U, Sigma and V for red matrix
+Ured, Sred, Vred = sp.linalg.svd(r) 
+#to find U, sigma and V for green matrix
+Ugreen, Sgreen, Vgreen = sp.linalg.svd(g) 
+#U, sigma and V for blue matrix
+Ublue, Sblue, Vblue = sp.linalg.svd(b) 
+
+#to find the non zero elements in sigma of each red, green and blue matrices
+nonzero_r=np.count_nonzero(Sred)
+nonzero_g=np.count_nonzero(Sgreen)
+nonzero_b=np.count_nonzero(Sblue)
+print("The number of non-zero elements in the original Sigma of red, green, blue matrices are", nonzero_r,"," ,nonzero_g,"and" ,nonzero_b, "respectively.")
+
+
+#lower resolution of sigma 30
+print("When n = 30, this is a lower resolution")
+svd_img(30)
+
+#better resolution of sigma 200
+print("When n = 200, this is a better resolution")
+svd_img(200)
